8c7db7c6edee367485dbc63a2deaec1b235a0335
glennka1337 <viktor.h@outlook.com>
2025-05-11 23:12:43 +0200

    Add project files.

---
+++ Labb4_ADV.NET/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Labb4_ADV.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibrarySystem library = new LibrarySystem();
+            UserInterface.DisplayMenu(library);
+        }
+    }
+}
